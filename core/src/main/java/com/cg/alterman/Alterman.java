7f41185e6d55981f873d0775734a70735c3b69d5
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2020-09-21 15:20:56 +0530

    Initial commit

---
+++ core/src/main/java/com/cg/alterman/Alterman.java
@@ -0,0 +1,11 @@
+package com.cg.alterman;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Alterman extends Game {
+	@Override
+	public void create() {
+		setScreen(new MainScreen());
+	}
+}
